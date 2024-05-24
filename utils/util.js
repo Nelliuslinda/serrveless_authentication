4630161b9802bd50808b2b2a12e8fc0295d61739
Nellius Kyalo <nelliuskyalo@gmail.com>
2024-05-24 14:57:30 +0300

    fix: (error) Fix error on index.js file

--- utils/util.js
+++ utils/util.js
@@ -1,11 +1,11 @@
-function buildResponse(statusCode, body){
-  return{
-      statusCode: statusCode,
-      headers: {
-        'Access-Control-Allow-Origin':'*',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(body)
+function buildResponse(statusCode, body) {
+  return {
+    statusCode: statusCode,
+    headers: {
+      'Access-Control-Allow-Origin' : '*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
   }
 }
 
